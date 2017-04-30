9a776af07258bff886c010c8a347702ac1a69fae
Andrew Lehman <aplehm@gmail.com>
2017-04-30 18:53:27 -0400

    Add methods to separate log by turn and fix duplication end of turn message error

--- classes/LogParser.php
+++ classes/LogParser.php
@@ -10,7 +10,11 @@ class LogParser {
     $this->log = $rawLog;
     $this->log = $this->parseHTML($this->log);
     $this->log = $this->separateLogByAction($this->log);
+    $this->log = $this->checkLogForDuplicateEndOfTurnErrors($this->log);
     $this->log = $this->cleanLog($this->log);
+    $this->log = $this->separateLogByTurn($this->log);
+    
+    echo json_encode($this->log);
   }
 
   // pulls text out of html
@@ -67,7 +71,50 @@ class LogParser {
         $action = str_replace('&nbsp;', ' ', $action);
         $action = preg_replace('/\,{2,}/', '', $action);
         $action = str_replace(' - ', '', $action);
-        echo $action . PHP_EOL;
+        $newLog[] = $action;
+      }
+    }
+
+    return $newLog;
+  }
+
+  //Added to catch bug discovered in log of game, where in the log a player had
+  //two end of turn statements (the two log statements were also out of order)
+  private function checkLogForDuplicateEndOfTurnErrors($log) {
+    $turnStartPattern = '/^BEGINNING OF TURN for ([a-zA-Z]+)/';
+    $turnEndPattern = '/^END OF TURN/';
+
+    for($index = 0; $index < count($log) - 1; $index += 1) {
+      $nextLog = $log[$index + 1];
+
+      //check that everytime we get the end of turn pattern the next line is the start
+      //turn pattern
+      if(preg_match($turnEndPattern, $log[$index]) === 1) {
+        if(preg_match($turnStartPattern, $nextLog) !== 1) {
+          $log[$index] = "";
+        }
+      }
+    }
+
+    return $log;
+  }
+
+  private function separateLogByTurn($log) {
+    $newLog = array();
+    $turnStartPattern = '/^BEGINNING OF TURN for ([a-zA-Z]+)/';
+    $turnCount = -1;
+
+    foreach($log as $action) {
+      $matches = array();
+
+      if(preg_match($turnStartPattern, $action, $matches) === 1) {
+        $turnCount += 1;
+        $newLog[$turnCount] = array(
+          'player' => $matches[1],
+          'actions' => array(),
+        );
+      } else {
+        $newLog[$turnCount]['actions'][] = $action;
       }
     }
 
