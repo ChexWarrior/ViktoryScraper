1ac1a068e7bd7501b04d82bdf96740a7648f8cd0
Andrew Lehman <aplehm@gmail.com>
2017-04-30 21:36:31 -0400

    Add methods to extract info on non-battle actions

--- classes/LogParser.php
+++ classes/LogParser.php
@@ -16,6 +16,7 @@ class LogParser {
     $this->log = $this->cleanLog($this->log);
     $this->log = $this->groupActionsByPlayer($this->log);
     $this->log = $this->determineGameTime($this->log, $this->playerInfo);
+    $this->log = $this->parseNonBattleActions($this->log);
     echo json_encode($this->log);
   }
 
@@ -240,5 +241,306 @@ class LogParser {
     }
 
     return $processedLog;
+  }
+
+  /**
+   * Uses regex to extract info of non-battle actions
+   */
+  private function parseNonBattleActions($log) {
+    $playerActions = array(
+      array(
+        'name' => 'revealedHexes',
+        'pattern' => '/Revealed ([0-9]+) hex(es)?/',
+        'data' => array(
+            'amount',
+        ),
+      ),
+      array(
+        'name' => 'unitsPlacedFromReserve',
+        'pattern' => '/Placed reserve (infantry|cavalry|artillery) in (town|city) at ([0-9]+)/',
+        'data' => array(
+            'unitType',
+            'structureType',
+            'targetHexID',
+        ),
+      ),
+      array(
+        'name' => 'unitsPlacedFromReserve',
+        'pattern' => '/Placed reserve (frigate) at ([0-9]+) for (town|city) at ([0-9]+)/',
+        'data' => array(
+            'unitType',
+            'targetHexID',
+            'structureType',
+            'structureHexID',
+        ),
+      ),
+      array(
+        'name' => 'hexPlaced',
+        'pattern' => '/Placed a (forest|water|mountain|grassland|plains) hex at ([0-9]+)/',
+        'data' => array(
+            'hexType',
+            'targetHexID',
+        ),
+      ),
+      array(
+        'name' => 'capitalPlaced',
+        'pattern' => '/Placed capital at ([0-9]+)/',
+        'data' => array(
+            'targetHexID',
+        ),
+      ),
+      array(
+        'name' => 'townPlaced',
+        'pattern' => '/Built town at ([0-9]+)/',
+        'data' => array(
+            'targetHexID',
+        ),
+      ),
+      array(
+        'name' => 'cityPlaced',
+        'pattern' => '/Upgraded town to city at ([0-9]+)/',
+        'data' => array(
+            'targetHexID',
+        ),
+      ),
+      array(
+        'name' => 'unitMoved',
+        'pattern' => '/Moved (infantry|cavalry|artillery|frigate) from ([0-9]+) ([0-9]+) spaces? to ([0-9]+)/',
+        'data' => array(
+            'unitType',
+            'sourceHexID',
+            'spacesMoved',
+            'targetHexID',
+        ),
+      ),
+      array(
+        'name' => 'tradeWaterHex',
+        'pattern' => '/Traded a water hex for a (forest|water|mountain|grassland|plains)/',
+        'data' => array(
+            'hexType',
+        ),
+      ),
+      array(
+        'name' => 'placeUnitInReserve',
+        'pattern' => '/Moved (infantry|cavalry|artillery|frigate) at ([0-9]+) to reserve/',
+        'data' => array(
+            'unitType',
+            'sourceHexID',
+        ),
+      ),
+      array(
+        'name' => 'loadUnitInFrigate',
+        'pattern' => '/Loaded (infantry|cavalry|artillery|frigate) at ([0-9]+) on to frigate at ([0-9]+)/',
+        'data' => array(
+            'unitType',
+            'sourceHexID',
+            'targetHexID',
+        ),
+      ),
+      array(
+        'name' => 'unloadUnitFromFrigate',
+        'pattern' => '/Unloaded (infantry|cavalry|artillery|frigate) from frigate at ([0-9]+) to ([0-9]+)/',
+        'data' => array(
+            'unitType',
+            'sourceHexID',
+            'targetHexID',
+        ),
+      ),
+      array(
+        'name' => 'playerResigned',
+        'pattern' => '/([a-zA-Z]+) resigns/',
+        'data' => array(
+            'player',
+        ),
+      ),
+      array(
+        'name' => 'bombard',
+        'pattern' => '/Bombarding \[([a-zA-Z]+)\] ([0-9]+) [a-zA-Z]+ units? in ([0-9]+) with (artillery|frigate) in ([0-9]+)/',
+        'data' => array(
+            'defender',
+            'unitAmount',
+            'defenderHexID',
+            'attackingUnitType',
+            'attackerHexID',
+        ),
+      ),
+      array(
+        'name' => 'attackRollNoHits',
+        'pattern' => '/([0-9]+) attacking units? rolled scoring no hits/',
+        'data' => array(
+          'unitAmount',
+        ),
+      ),
+      array(
+        'name' => 'attackRoll',
+        'pattern' => '/([0-9]+) attacking units? rolled scoring ([0-9]+) tactical and ([0-9]+) nontactical hits?\./',
+        'data' => array(
+          'unitAmount',
+          'tacticalHits',
+          'nontacticalHits',
+        ),
+      ),
+      array(
+        'name' => 'attackRoll',
+        'pattern' => '/([0-9]+) attacking units? rolled scoring ([0-9]+) tactical hits?\./',
+        'data' => array(
+          'unitAmount',
+          'tacticalHits',
+        ),
+      ),
+      array(
+        'name' => 'attackRoll',
+        'pattern' => '/([0-9]+) attacking units? rolled scoring ([0-9]+) nontactical hits?\./',
+        'data' => array(
+          'unitAmount',
+          'nontacticalHits',
+        ),
+      ),
+      array(
+        'name' => 'attackRoll',
+        'pattern' => '/([0-9]+) attacking units? rolled scoring ([0-9]+) nontactical hits? supp?ressing ([0-9]+) defensive rolls?\./',
+        'data' => array(
+          'unitAmount',
+          'nontacticalHits',
+          'suppressedRolls',
+        ),
+      ),
+      array(
+        'name' => 'attackRoll',
+        'pattern' => '/([0-9]+) attacking units? rolled scoring ([0-9]+) tactical hits? supp?ressing ([0-9]+) defensive rolls?\./',
+        'data' => array(
+          'unitAmount',
+          'tacticalHits',
+          'suppressedRolls',
+        ),
+      ),
+      array(
+        'name' => 'attackRoll',
+        'pattern' => '/([0-9]+) attacking units? rolled scoring ([0-9]+) tactical and ([0-9]+) nontactical hits? supp?ressing ([0-9]+) defensive rolls?\./',
+        'data' => array(
+          'unitAmount',
+          'tacticalHits',
+          'nontacticalHits',
+          'suppressedRolls',
+        ),
+      ),
+      array(
+        'name' => 'defenderRollNoHits',
+        'pattern' => '/([0-9]+) defending units? rolled scoring no hits/',
+        'data' => array(
+          'unitAmount',
+        ),
+      ),
+      array(
+        'name' => 'defenderRoll',
+        'pattern' => '/([0-9]+) defending units? rolled scoring ([0-9]+) tactical and ([0-9]+) nontactical hits?\./',
+        'data' => array(
+          'unitAmount',
+          'tacticalHits',
+          'nontacticalHits',
+        ),
+      ),
+      array(
+        'name' => 'defenderRoll',
+        'pattern' => '/([0-9]+) defending units? rolled scoring ([0-9]+) tactical hits?\./',
+        'data' => array(
+          'unitAmount',
+          'tacticalHits',
+        ),
+      ),
+      array(
+        'name' => 'defenderRoll',
+        'pattern' => '/([0-9]+) defending units? rolled scoring ([0-9]+) nontactical hits?\./',
+        'data' => array(
+          'unitAmount',
+          'nontacticalHits',
+        ),
+      ),
+      array(
+        'name' => 'battleStart',
+        'pattern' => '/\[([a-zA-Z]+)\] Resolving battle in ([0-9]+)/',
+        'data' => array(
+          'defenderColor',
+          'targetHexID',
+        ),
+      ),
+      array(
+        'name' => 'battleResult',
+        'pattern' => '/(Attacker|Defender) returned ([0-9]+) (infantry|cavalry|artillery|frigates?)(?: and)?( [0-9]+)?( infantry| cavalry| artillery| frigates?)?(?: and)?( [0-9]+)?( infantry| cavalry| artillery| frigates?)?(?: and)?( [0-9]+)?( infantry| cavalry| artillery| frigates?)? to their reserve/',
+        'data' => array(
+          'target',
+          'amountLost1',
+          'unitType1',
+          'amountLost2',
+          'unitType2',
+          'amountLost3',
+          'unitType3',
+          'amountLost4',
+          'unitType4',
+        ),
+      ),
+      array(
+        'name' => 'battleEnd',
+        'pattern' => '/(Attacker|Defender) prevails (?:retains|gains) control of hex ([0-9]+)/',
+        'data' => array(
+          'victor',
+          'targetHexID',
+        ),
+      ),
+      array(
+        'name' => 'battleEnd',
+        'pattern' => '/Both sides eliminated hex ([0-9]+)/',
+        'data' => array(
+          'targetHexID',
+        ),
+      ),
+      array(
+        'name' => 'retreat',
+        'pattern' => '/Retreated (infantry|cavalry|artillery|frigate) from ([0-9]+) to ([0-9]+)/',
+        'data' => array(
+          'unitType',
+          'sourceHexID',
+          'targetHexID',
+        ),
+      ),
+    );
+
+    foreach($log as &$playerTurn) {
+      foreach($playerTurn['actions'] as &$action) {
+        $origAction = $action;
+        foreach($playerActions as $playerAction) {
+          $action = $this->parseNonBattleActionStats(
+              $action,
+              $playerAction['pattern'],
+              $playerAction['name'],
+              $playerAction['data']
+            );
+
+          if($action !== $origAction) break;
+        }
+      }
+    }
+
+    return $log;
+  }
+
+  /**
+   * Stores information about each action as array
+   */
+  private function parseNonBattleActionStats($actionString, $pattern, $actionName, $data) {
+    $matches = array();
+    $result = array();
+
+    if(preg_match($pattern, $actionString, $matches) === 1) {
+      $result['type'] = $actionName;
+
+      for($index = 0; $index < count($data) && $index < count($matches) - 1; $index += 1) {
+        $result[$data[$index]] = $matches[$index + 1];
+      }
+
+      return $result;
+    }
+
+    return $actionString;
   }
 }
